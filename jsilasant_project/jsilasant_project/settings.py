91bbf516e7aa8ed2ba0f8ef01256416e20dd0849
Jsilasant <36745524+Rekalous@users.noreply.github.com>
2020-09-18 04:06:27 -0500

    finished profile,pictures and automatic profiles

--- jsilasant_project/jsilasant_project/settings.py
+++ jsilasant_project/jsilasant_project/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,5 +127,7 @@ STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
-
 LOGIN_URL = 'login'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
